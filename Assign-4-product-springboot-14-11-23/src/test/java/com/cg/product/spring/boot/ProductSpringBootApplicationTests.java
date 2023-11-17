bd5d19b8938c24fb6c059745611c54d9d5c68a6f
GosalaAlpha <149370375+GosalaAlpha@users.noreply.github.com>
2023-11-17 09:17:17 +0530

    Add files via upload

---
+++ Assign-4-product-springboot-14-11-23/src/test/java/com/cg/product/spring/boot/ProductSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.product.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
